75353c79d9b87c4618428cea281533f68520dc6e
codyhelm94 <cody.d.helm@gmail.com>
2022-02-07 20:24:51 -0600

    Corrected Assert

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,11 +1,10 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.*;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -17,20 +16,20 @@ public class JobTest {
     public void testSettingJobId() {
         Job jobOne=new Job();
         Job jobTwo=new Job();
-        assertEquals(jobOne.getId()+1,jobTwo.getId(),.001);
+        Assert.assertEquals(  jobOne.getId()+1,jobTwo.getId(),.001);
     }
 
     @Test
     public void testJobConstructorSetsAllFields(){
         Job testJob=new Job("Product tester",new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality Control"),new CoreCompetency("Persistence"));
-        assertTrue(testJob instanceof Job);
-        assertEquals("Product tester",testJob.getName());
-        assertEquals("ACME",testJob.getEmployer().getValue());
-        assertEquals("Desert",testJob.getLocation().getValue());
-        assertEquals("Quality Control",testJob.getPositionType().getValue());
-        assertEquals("Persistence",testJob.getCoreCompetency().getValue());
-        assertEquals("Product tester", testJob.getName());
+        Assert.assertTrue(testJob instanceof Job);
+        Assert.assertEquals("Product tester",testJob.getName());
+        Assert.assertEquals("ACME",testJob.getEmployer().getValue());
+        Assert.assertEquals("Desert",testJob.getLocation().getValue());
+        Assert.assertEquals("Quality Control",testJob.getPositionType().getValue());
+        Assert.assertEquals("Persistence",testJob.getCoreCompetency().getValue());
+        Assert.assertEquals("Product tester", testJob.getName());
     }
 
 
@@ -40,7 +39,7 @@ public class JobTest {
                 new PositionType("Quality Control"),new CoreCompetency("Persistence"));
         Job testTwo=new Job("Product tester",new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality Control"),new CoreCompetency("Persistence"));
-        assertFalse(testTwo==testOne);
+        Assert.assertFalse(testTwo==testOne);
     }
 
     @Test
@@ -54,20 +53,20 @@ public class JobTest {
         Job testFour=new Job("",new Employer(""), new Location(""),
                 new PositionType(""),new CoreCompetency(""));
 
-        assertEquals('\n',testOne.toString().charAt(0));
-        assertEquals('\n',testOne.toString().charAt(testOne.toString().length()-1));
-        assertTrue(testOne.toString().contains("\nID: "+testOne.getId()));
-        assertTrue(testOne.toString().contains("\nName: "+testOne.getName()));
-        assertTrue(testOne.toString().contains("\nEmployer: "+testOne.getEmployer().getValue()));
-        assertTrue(testOne.toString().contains("\nLocation: "+testOne.getLocation().getValue()));
-        assertTrue(testOne.toString().contains("\nPosition Type: "+testOne.getPositionType()));
-        assertTrue(testOne.toString().contains("\nCore Competency: "+testOne.getCoreCompetency().getValue()));
-        assertTrue(testThree.toString().contains("\nName: Data not available"));
-        assertTrue(testTwo.toString().contains("\nEmployer: Data not available"));
-        assertTrue(testTwo.toString().contains("\nLocation: Data not available"));
-        assertTrue(testTwo.toString().contains("\nPosition Type: Data not available"));
-        assertTrue(testTwo.toString().contains("\nCore Competency: Data not available"));
-        assertEquals("OOPS! This job does not seem to exist.",testFour.toString());
+        Assert.assertEquals('\n',testOne.toString().charAt(0));
+        Assert.assertEquals('\n',testOne.toString().charAt(testOne.toString().length()-1));
+        Assert.assertTrue(testOne.toString().contains("\nID: "+testOne.getId()));
+        Assert.assertTrue(testOne.toString().contains("\nName: "+testOne.getName()));
+        Assert.assertTrue(testOne.toString().contains("\nEmployer: "+testOne.getEmployer().getValue()));
+        Assert.assertTrue(testOne.toString().contains("\nLocation: "+testOne.getLocation().getValue()));
+        Assert.assertTrue(testOne.toString().contains("\nPosition Type: "+testOne.getPositionType()));
+        Assert.assertTrue(testOne.toString().contains("\nCore Competency: "+testOne.getCoreCompetency().getValue()));
+        Assert.assertTrue(testThree.toString().contains("\nName: Data not available"));
+        Assert.assertTrue(testTwo.toString().contains("\nEmployer: Data not available"));
+        Assert.assertTrue(testTwo.toString().contains("\nLocation: Data not available"));
+        Assert.assertTrue(testTwo.toString().contains("\nPosition Type: Data not available"));
+        Assert.assertTrue(testTwo.toString().contains("\nCore Competency: Data not available"));
+        Assert.assertEquals("OOPS! This job does not seem to exist.",testFour.toString());
     }
 
 
